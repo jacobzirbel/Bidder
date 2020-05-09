2e93245193189abee47fde54196b62c51fd3148f
Jacob Zirbel <jacobzirbel@gmail.com>
2020-05-09 14:55:06 -0500

    add create user function

--- auction.js
+++ auction.js
@@ -5,12 +5,15 @@ const connection = mysql.createConnection({
 	host: info.host,
 	port: 3306,
 	user: info.username,
-	password: info.username,
+	password: info.password,
 	database: info.dbName,
 });
-
+connection.connect((err) => {
+	if (err) throw err;
+	console.log("connected to db");
+	startUp();
+});
 let currentUser;
-
 function startUp() {
 	inquirer
 		.prompt([
@@ -24,43 +27,51 @@ function startUp() {
 				],
 			},
 		])
-		.then((response) => {
+		.then(async (response) => {
 			if (response.login === 1) {
-				attemptLogin();
+				currentUser = await attemptLogin();
+				console.log(currentUser);
 			} else {
-				createAccount();
+				userCreateAccount();
 			}
 		});
 }
 
 async function attemptLogin() {
 	let users = getUsers();
-	console.log(users);
 	let attempted;
+	if (!users) {
+		console.log("There Are no Users!");
+		startUp();
+		return;
+	}
 	let response = await inquirer.prompt([
 		{
 			name: "try",
-			message: "attempt login?",
+			message: "Login?",
 			type: "list",
-			choices: ["Yes", "No"],
+			choices: [
+				{ name: "Yes", value: true },
+				{ name: "No", value: false },
+			],
 		},
 		{
 			name: "username",
 			message: "username?",
-			when: (answers) => answers.try === "Yes",
+			when: (answers) => answers.try,
 		},
 		{
 			name: "password",
 			message: "password?",
-			when: (answers) => answers.try === "Yes",
+			type: "password",
+			when: (answers) => answers.try,
 		},
 	]);
 
 	let selectedUser = users.find((e) => e.username === response.username);
 	if (selectedUser) {
 		if (response.password === selectedUser.password) {
-			currentUser = selectedUser;
-			return;
+			return selectedUser;
 		} else {
 			console.log("Not a valid username/password combination");
 			attempted = true;
@@ -72,13 +83,73 @@ async function attemptLogin() {
 		attemptLogin();
 	}
 }
+
+function userCreateAccount() {
+	let users = getUsers();
+	inquirer
+		.prompt([
+			{
+				name: "try",
+				message: "Create Account?",
+				type: "list",
+				choices: [
+					{ name: "Yes", value: true },
+					{ name: "No", value: false },
+				],
+			},
+			{
+				name: "username",
+				message: "Username?",
+				when: (answers) => answers.try,
+			},
+			{
+				name: "password",
+				message: "Password?",
+				when: (answers) => answers.try,
+			},
+			{
+				name: "check",
+				message: "Password again?",
+				when: (answers) => answers.try,
+			},
+		])
+		.then((response) => {
+			if (response.username.length < 1) {
+				console.log("You must enter a username!");
+				userCreateAccount();
+			}
+			if (users.find((e) => e.username === response.username)) {
+				console.log("Username taken!");
+				userCreateAccount();
+			}
+			if (response.password === response.check) {
+				dbCreateAccount(response.username, response.password);
+			} else {
+				console.log("Passwords must match!");
+				userCreateAccount();
+			}
+		});
+}
 function getUsers() {
 	let users = [];
 	connection.query("SELECT * FROM users", (err, res) => {
 		if (err) throw err;
 		res.forEach((e) =>
 			// Probably not great to store password like this but here we are
-			users.push({ id: e.id, name: e.name, password: e.password })
+			users.push({ id: e.id, username: e.username, password: e.password })
 		);
 	});
+	return users;
+}
+
+function dbCreateAccount(username, password) {
+	const query = connection.query(
+		"INSERT INTO users SET ?",
+		{ username, password },
+		(err, res) => {
+			if (err) throw err;
+			console.log("user added!");
+			console.log(getUsers());
+		}
+	);
 }
